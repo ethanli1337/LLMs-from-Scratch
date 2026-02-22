75ba421d62b6f55df758e82061c86c79235757cc
Ethan Li <ethanli2756@gmail.com>
2026-02-22 12:30:53 -0600

    tensor basics 2

--- tensor_basics.py
+++ tensor_basics.py
@@ -1,5 +1,5 @@
 import torch 
-
+import numpy as np
 x=torch.empty(2,2,2,3)
 y=torch.randn(2,2)
 z=torch.zeros(2,2,dtype=torch.float16)
@@ -13,5 +13,22 @@ p=torch.mul(c,d)
 o=torch.rand(5,3)
 v=torch.rand(4,4)
 p=v.view(-1,8)
-print(p.size())
-print(o[1,1].item())
+
+a=torch.ones(5)
+b=a.numpy()
+a.add_(1)
+a=np.ones(5)
+b=torch.from_numpy(a)
+
+a+=1
+
+if torch.cuda.is_available():
+    device=torch.device("cuda")
+    x=torch.ones(5, device=device)
+    y=torch.ones(5)
+    y=y.to(device)
+    z=x+y
+    z=z.to("cpu")
+
+x=torch.ones(5,requires_grad=True)
+print(x)
