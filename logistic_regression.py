a3e423ca74a4de088aca671a8dd1906b1289d82a
Ethan Li <ethanli2756@gmail.com>
2026-03-02 17:32:47 -0600

    fix: correct X_test transformation and enhance training loop structure

--- logistic_regression.py
+++ logistic_regression.py
@@ -22,7 +22,7 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_
 #scale 
 sc=StandardScaler()
 X_train=sc.fit_transform(X_train)
-X_test=sc.transform()
+X_test=sc.transform(X_test)
 
 X_train=torch.from_numpy(X_train.astype(np.float32))
 X_test=torch.from_numpy(X_test.astype(np.float32))
@@ -45,6 +45,30 @@ class LogisticRegression(nn.Module):
         y_predicted=torch.sigmoid(self.linear(x))
         return y_predicted 
 
-#2 loss and optimizer 
-#3 training loop 
 
+model=LogisticRegression(n_features)
+#2 loss and optimizer 
+learning_rate=0.01
+criterion=nn.BCELoss()
+optimizer=torch.optim.SGD(model.parameters(), lr=learning_rate)
+#3 training loop 
+num_epochs=100
+for epoch in range(num_epochs):
+    # forward pass and loss
+    y_predicted=model(X_train)
+    loss=criterion(y_predicted,y_train)
+    # backward pass
+    loss.backward()
+    # updates
+    optimizer.step()
+    # zero gradients 
+    optimizer.zero_grad()
+
+    if (epoch+1) % 10 == 0:
+        print(f'epoch: {epoch+1}, loss = {loss.item():.4f}')
+
+with torch.no_grad():
+    y_predicted = model(X_test)
+    y_predicted_cls = y_predicted.round() 
+    acc = y_predicted_cls.eq(y_test).sum() / float(y_test.shape[0])
+    print(f'accuracy = {acc:.4f}') 
