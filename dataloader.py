66568855fc6875d8e201f73093b635d3b10860d5
Ethan Li <ethanli2756@gmail.com>
2026-03-06 17:33:52 -0600

    add dataset transforms for WineDataset with ToTensor and MulTransform classes

--- dataloader.py
+++ dataloader.py
@@ -35,4 +35,4 @@ for epoch in range(num_epochs):
         if (i+1) % 5 == 0:
             print(f'epoch {epoch+1}/{num_epochs}, step {i+1}/{n_iterations}, inputs {inputs.shape}') 
         
-torchvision.datasets.MNIST()
+
