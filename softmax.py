13f7719131cc80d5779c0565f1f6e4f157e6d51f
Ethan Li <ethanli2756@gmail.com>
2026-03-07 17:40:17 -0600

    refactor: update softmax and cross-entropy examples for clarity and consistency

--- softmax.py
+++ softmax.py
@@ -7,11 +7,11 @@ def softmax(x):
 
 x=np.array([2.0,1.0,0.1])
 outputs=softmax(x)
-print(outputs)
+# print(outputs)
 
 x=torch.tensor([2.0,1.0,0.1])
 outputs=torch.softmax(x, dim=0)
-print(outputs)
+# print(outputs)
 
 def cross_entropy(actual, predicted):
     loss=-np.sum(actual*np.log(predicted))
@@ -23,13 +23,24 @@ Y_pred_good=np.array([0.7,0.2,0.1])
 Y_pred_bad=np.array([0.1,0.3,0.6])
 l1=cross_entropy(Y, Y_pred_good)
 l2=cross_entropy(Y, Y_pred_bad)
-print(f"{l1:.4f}")
-print(f"{l2:.4f}")
+# print(f"{l1:.4f}")
+# print(f"{l2:.4f}")
 
 loss=nn.CrossEntropyLoss()
 
-Y=torch.tensor([0])
+# 3 samples
+Y=torch.tensor([2,0,1])
+
 # nsamples x nclasses
-Y_pred_good=torch.tensor([[2.0,1.0,0.1]])
-Y_pred_bad=torch.tensor([[0.5,2.0,0.3]])
+Y_pred_good=torch.tensor([[0.1,1.0,2.1],[2.0,1.0,0.1],[2.0,3.0,0.1]])
+Y_pred_bad=torch.tensor([[2.1,1.0,0.1], [0.1,1.0,2.1], [0.1,3.0,0.1]])
+l1=loss(Y_pred_good, Y)
+l2=loss(Y_pred_bad, Y)
+print(l1.item())
+print(l2.item())
+
+_, predictions1=torch.max(Y_pred_good, 1)
+_, predictions2=torch.max(Y_pred_bad, 1)
+print(predictions1)
+print(predictions2)
 
