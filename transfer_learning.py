db448a24e8026a8dce58abf932c1d267522d18c0
Ethan Li <ethanli2756@gmail.com>
2026-03-14 17:30:47 -0500

    refactor: streamline training loop and model fine-tuning in transfer learning script

--- transfer_learning.py
+++ transfer_learning.py
@@ -58,128 +58,110 @@ inputs, classes = next(iter(dataloaders['train']))
 imshow(inputs[0],title=class_names[classes[0]])
 # Make a grid from batch
 
-# out = torchvision.utils.make_grid(inputs)
-# imshow(out, title=[class_names[x] for x in classes])
+out = torchvision.utils.make_grid(inputs)
+imshow(out, title=[class_names[x] for x in classes])
 
-# def train_model(model, criterion, optimizer, scheduler, num_epochs=25):
-#     since = time.time()
+def train_model(model, criterion, optimizer, scheduler, num_epochs=25):
+    since = time.time()
 
-#     best_model_wts = copy.deepcopy(model.state_dict())
-#     best_acc = 0.0
+    best_model_wts = copy.deepcopy(model.state_dict())
+    best_acc = 0.0
 
-#     for epoch in range(num_epochs):
-#         print('Epoch {}/{}'.format(epoch, num_epochs - 1))
-#         print('-' * 10)
+    for epoch in range(num_epochs):
+        print('Epoch {}/{}'.format(epoch, num_epochs - 1))
+        print('-' * 10)
 
-#         # Each epoch has a training and validation phase
-#         for phase in ['train', 'val']:
-#             if phase == 'train':
-#                 model.train()  # Set model to training mode
-#             else:
-#                 model.eval()   # Set model to evaluate mode
+        # Each epoch has a training and validation phase
+        for phase in ['train', 'val']:
+            if phase == 'train':
+                model.train()  # Set model to training mode
+            else:
+                model.eval()   # Set model to evaluate mode
 
-#             running_loss = 0.0
-#             running_corrects = 0
+            running_loss = 0.0
+            running_corrects = 0
 
-#             # Iterate over data.
-#             for inputs, labels in dataloaders[phase]:
-#                 inputs = inputs.to(device)
-#                 labels = labels.to(device)
+            # Iterate over data.
+            for inputs, labels in dataloaders[phase]:
+                inputs = inputs.to(device)
+                labels = labels.to(device)
 
-#                 # forward
-#                 # track history if only in train
-#                 with torch.set_grad_enabled(phase == 'train'):
-#                     outputs = model(inputs)
-#                     _, preds = torch.max(outputs, 1)
-#                     loss = criterion(outputs, labels)
+                # forward
+                # track history if only in train
+                with torch.set_grad_enabled(phase == 'train'):
+                    outputs = model(inputs)
+                    _, preds = torch.max(outputs, 1)
+                    loss = criterion(outputs, labels)
 
-#                     # backward + optimize only if in training phase
-#                     if phase == 'train':
-#                         optimizer.zero_grad()
-#                         loss.backward()
-#                         optimizer.step()
+                    # backward + optimize only if in training phase
+                    if phase == 'train':
+                        optimizer.zero_grad()
+                        loss.backward()
+                        optimizer.step()
 
-#                 # statistics
-#                 running_loss += loss.item() * inputs.size(0)
-#                 running_corrects += torch.sum(preds == labels.data)
+                # statistics
+                running_loss += loss.item() * inputs.size(0)
+                running_corrects += torch.sum(preds == labels.data)
 
-#             if phase == 'train':
-#                 scheduler.step()
+            if phase == 'train':
+                scheduler.step()
 
-#             epoch_loss = running_loss / dataset_sizes[phase]
-#             epoch_acc = running_corrects.double() / dataset_sizes[phase]
+            epoch_loss = running_loss / dataset_sizes[phase]
+            epoch_acc = running_corrects.double() / dataset_sizes[phase]
 
-#             print('{} Loss: {:.4f} Acc: {:.4f}'.format(
-#                 phase, epoch_loss, epoch_acc))
+            print('{} Loss: {:.4f} Acc: {:.4f}'.format(
+                phase, epoch_loss, epoch_acc))
 
-#             # deep copy the model
-#             if phase == 'val' and epoch_acc > best_acc:
-#                 best_acc = epoch_acc
-#                 best_model_wts = copy.deepcopy(model.state_dict())
+            # deep copy the model
+            if phase == 'val' and epoch_acc > best_acc:
+                best_acc = epoch_acc
+                best_model_wts = copy.deepcopy(model.state_dict())
 
-#         print()
+        print()
 
-#     time_elapsed = time.time() - since
-#     print('Training complete in {:.0f}m {:.0f}s'.format(
-#         time_elapsed // 60, time_elapsed % 60))
-#     print('Best val Acc: {:4f}'.format(best_acc))
+    time_elapsed = time.time() - since
+    print('Training complete in {:.0f}m {:.0f}s'.format(
+        time_elapsed // 60, time_elapsed % 60))
+    print('Best val Acc: {:4f}'.format(best_acc))
 
-#     # load best model weights
-#     model.load_state_dict(best_model_wts)
-#     return model
-
-
-# #### Finetuning the convnet ####
-# # Load a pretrained model and reset final fully connected layer.
-
-# model = models.resnet18(pretrained=True)
-# num_ftrs = model.fc.in_features
-# # Here the size of each output sample is set to 2.
-# # Alternatively, it can be generalized to nn.Linear(num_ftrs, len(class_names)).
-# model.fc = nn.Linear(num_ftrs, 2)
-
-# model = model.to(device)
-
-# criterion = nn.CrossEntropyLoss()
-
-# # Observe that all parameters are being optimized
-# optimizer = optim.SGD(model.parameters(), lr=0.001)
-
-# # StepLR Decays the learning rate of each parameter group by gamma every step_size epochs
-# # Decay LR by a factor of 0.1 every 7 epochs
-# # Learning rate scheduling should be applied after optimizer’s update
-# # e.g., you should write your code this way:
-# # for epoch in range(100):
-# #     train(...)
-# #     validate(...)
-# #     scheduler.step()
-
-# step_lr_scheduler = lr_scheduler.StepLR(optimizer, step_size=7, gamma=0.1)
-
-# model = train_model(model, criterion, optimizer, step_lr_scheduler, num_epochs=25)
+    # load best model weights
+    model.load_state_dict(best_model_wts)
+    return model
 
 
-# #### ConvNet as fixed feature extractor ####
-# # Here, we need to freeze all the network except the final layer.
-# # We need to set requires_grad == False to freeze the parameters so that the gradients are not computed in backward()
-# model_conv = torchvision.models.resnet18(pretrained=True)
-# for param in model_conv.parameters():
-#     param.requires_grad = False
+#### Finetuning the convnet ####
+# Load a pretrained model and reset final fully connected layer.
 
-# # Parameters of newly constructed modules have requires_grad=True by default
-# num_ftrs = model_conv.fc.in_features
-# model_conv.fc = nn.Linear(num_ftrs, 2)
+model=models.resnet18(pretrained=True)
+num_ftrs=model.fc.in_features 
 
-# model_conv = model_conv.to(device)
+model.fc=nn.Linear(num_ftrs,2)
+model.to(device)
 
-# criterion = nn.CrossEntropyLoss()
+criterion=nn.CrossEntropyLoss()
+optimizer=optim.SGD(model.parameters(),lr=0.001)
 
-# # Observe that only parameters of final layer are being optimized as
-# # opposed to before.
-# optimizer_conv = optim.SGD(model_conv.fc.parameters(), lr=0.001, momentum=0.9)
+# scheduler
 
-# # Decay LR by a factor of 0.1 every 7 epochs
-# exp_lr_scheduler = lr_scheduler.StepLR(optimizer_conv, step_size=7, gamma=0.1)
+step_lr_scheduler=lr_scheduler.StepLR(optimizer,step_size=7,gamma=0.1)
 
-# model_conv = train_model(model_conv, criterion, optimizer_conv,
-#                          exp_lr_scheduler, num_epochs=25)
+model=train_model(model,criterion,optimizer,step_lr_scheduler,num_epochs=20)
+
+#####
+
+model=models.resnet18(pretrained=True)
+for param in model.parameters():
+    param.requires_grad=False
+num_ftrs=model.fc.input_features 
+
+model.fc=nn.Linear(num_ftrs,2)
+model.to(device)
+
+criterion=nn.CrossEntropyLoss()
+optimizer=optim.SGD(model.parameters(),lr=0.001)
+
+# scheduler
+
+step_lr_scheduler=lr_scheduler.StepLR(optimizer,step_size=7,gamma=0.1)
+
+model=train_model(model,criterion,optimizer,step_lr_scheduler,num_epochs=20)
